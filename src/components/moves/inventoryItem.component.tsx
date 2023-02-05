d4f4a462cd8630ff15cf2d3c3d1b133129aac693
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2023-02-05 21:17:46 +0200

    Added international move journey

--- src/components/moves/inventoryItem.component.tsx
+++ src/components/moves/inventoryItem.component.tsx
@@ -1,21 +1,42 @@
-import React from 'react'
-import { Form, FormGroup } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
+import useAPI from 'src/_hooks/useAPI';
+import useNumberInput from "../../_hooks/useNumberInput";
 
 type Props = {
     item: any;
 }
 
 function InventoryItem({ item }: Props) {
-  return (
-    <div className='InventoryItem'>
-        <Form.Label>
-            <Form.Control
-                type='checkbox'
-            />
-            { item.title }
-        </Form.Label>
+  const api = useAPI();
+  const { ValueDisplay: itemCount, Value: itemCountValue } = useNumberInput();
 
-    </div>
+  const handleSelect = async (item: any) => {
+    console.log(`Inventory Item`, item.target.id);
+
+    const data = {
+      item: item.target.id,
+      booking: ''
+    }
+
+    const response = await api.post('/inventory', data);
+    console.log(response)
+  }
+
+  return (
+    <Row className='InventoryItem'>
+      <Col>
+        <Form.Check
+          inline
+          type='checkbox'
+          label={ item.title }
+          id={item.id}
+          onChange={handleSelect}
+        />
+      </Col>
+      <Col>
+        { itemCount }
+      </Col>
+    </Row>
   )
 }
 
