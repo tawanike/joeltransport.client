72f64a6269bc9d77f16b3562f1e41868c5c39f91
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2023-03-31 04:33:40 +0200

    Update landing page

--- src/_configurations/selectors.config.tsx
+++ src/_configurations/selectors.config.tsx
@@ -1,31 +1,32 @@
 export type SelectorConfig = {
-    id: number,
-    title: string,
-    route: string,
-    heading: string,
-    description: string
+  id: number;
+  title: string;
+  route: string;
+  heading: string;
+  description: string;
 };
 
 export const selectorConfig: SelectorConfig[] = [
-    {
-        id: 1,
-        title: "Home Move",
-        route: "/move/domestic",
-        heading: "Get free quote for your home move",
-        description: "Get free quote for your home move"
-    },
-    // {
-    //     id: 2,
-    //     title: "International",
-    //     route: "/move/international",
-    //     heading: "Looking for long distance moving services?",
-    //     description: "For long distance & international services, we’ll need to collect your info & one of our specialist will be in touch."
-    // },
-    {
-        id: 3,
-        title: "Storage",
-        route: "/storage",
-        heading: "Looking for storage? Get an instant quote.",
-        description: "Get your free instant quote in Gauteng, for Kwazulu-Natal, or Cape Town please give us your contact information and a specialist will assist you."
-    }
+  {
+    id: 1,
+    title: "Home move",
+    route: "/move/domestic",
+    heading: "Chat to us about how we can simplify your process.",
+    description: "Get free quote for your home move",
+  },
+  // {
+  //     id: 2,
+  //     title: "International",
+  //     route: "/move/international",
+  //     heading: "Looking for long distance moving services?",
+  //     description: "For long distance & international services, we’ll need to collect your info & one of our specialist will be in touch."
+  // },
+  {
+    id: 3,
+    title: "Storage",
+    route: "/storage",
+    heading: "Looking for storage? Get an instant quote.",
+    description:
+      "Get your free instant quote in Gauteng, for Kwazulu-Natal, or Cape Town please give us your contact information and a specialist will assist you.",
+  },
 ];
