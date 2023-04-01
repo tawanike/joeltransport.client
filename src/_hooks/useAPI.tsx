82dd3d7e923d82753481a95ea51eb1d2772934aa
Tawanda Abraham Makunike <tawanda@mmogomedia.com>
2023-04-01 17:18:31 +0200

    Update pricing

--- src/_hooks/useAPI.tsx
+++ src/_hooks/useAPI.tsx
@@ -1,62 +1,50 @@
-import { useContext } from "react";
-import UserAuthStateContext from "../_contexts/userAuth.context";
-import { LOGOUT, RequestOptions } from "../_models/types";
+import { RequestOptions } from "../_models/types";
 
 const useAPI = () => {
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    const { UserAuthState, dispatchUserAuth } = useContext(UserAuthStateContext);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-    const request = (method: string) => {
-        return (url: string, body: any) => {
-            const requestOptions = {
-                method,
-                headers: authHeader(url)
-            } as RequestOptions;
-            if (body) {
-                requestOptions.headers['Content-Type'] = 'application/json';
-                requestOptions.body = JSON.stringify(body);
-            }
-            return fetch(API_URL + url, requestOptions).then(handleResponse);
-        }
-    }
-
-    const authHeader = (url: string) => {
-        const token = authToken();
-        const isLoggedIn = !!token;
-        const isApiUrl = url.startsWith(process.env.NEXT_PUBLIC_API_URL as string);
-        if (isLoggedIn && isApiUrl) {
-            return { Authorization: `Bearer ${token}` };
-        } else {
-            return {};
-        }
-    }
-
-    const authToken = () => {
-        return UserAuthState?.token;
-    }
-
-    const handleResponse = (response: any) => {
-        return response.text().then((text: any) => {
-            const data = text && JSON.parse(text);
-            if (!response.ok) {
-                if ([401, 403].includes(response.status) && authToken()) {
-                    dispatchUserAuth({
-                        type: LOGOUT
-                    });
-                }
-                const error = (data && (data.detail ?? data)) || response.statusText;
-                return Promise.reject(error);
-            }
-            return data;
-        });
-    }
-    return {
-        get: request('GET'),
-        post: request('POST'),
-        put: request('PUT'),
-        patch: request('PATCH'),
-        delete: request('DELETE')
+  const request = (method: string) => {
+    return (url: string, body: any) => {
+      const requestOptions = {
+        method,
+        headers: authHeader(url),
+      } as RequestOptions;
+      if (body) {
+        requestOptions.headers["Content-Type"] = "application/json";
+        requestOptions.body = JSON.stringify(body);
+      }
+      return fetch(API_URL + url, requestOptions).then(handleResponse);
     };
-}
+  };
+
+  const authHeader = (url: string) => {
+    const token = "";
+    const isLoggedIn = !!token;
+    const isApiUrl = url.startsWith(process.env.NEXT_PUBLIC_API_URL as string);
+    if (isLoggedIn && isApiUrl) {
+      return { Authorization: `Bearer ${token}` };
+    } else {
+      return {};
+    }
+  };
+
+  const handleResponse = (response: any) => {
+    return response.text().then((text: any) => {
+      const data = text && JSON.parse(text);
+      if (!response.ok) {
+        const error = (data && (data.detail ?? data)) || response.statusText;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+  };
+  return {
+    get: request("GET"),
+    post: request("POST"),
+    put: request("PUT"),
+    patch: request("PATCH"),
+    delete: request("DELETE"),
+  };
+};
 
 export default useAPI;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,48 +5,38 @@ import { SSRProvider } from "react-bootstrap";
 import "react-phone-number-input/style.css";
 import BookingContextProvider from "src/_contexts/booking.context";
 import {
-    CostSummaryReducer,
-    CostSummaryStateProvider,
-    initialCostSummaryState,
+  CostSummaryReducer,
+  CostSummaryStateProvider,
+  initialCostSummaryState,
 } from "../_contexts/costSummary.context";
-import {
-    initialUserAuthState,
-    UserAuthReducer,
-    UserAuthStateProvider,
-} from "../_contexts/userAuth.context";
+
 import { Layout } from "../components/Layouts";
 import "../styles/styles.scss";
 
 const OpenSans = Open_Sans({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-    const [UserAuthState, dispatchUserAuth] = useReducer(
-        UserAuthReducer,
-        initialUserAuthState
-    );
-    const [CostSummaryState, dispatchCostSummary] = useReducer(
-        CostSummaryReducer,
-        initialCostSummaryState
-    );
+  const [CostSummaryState, dispatchCostSummary] = useReducer(
+    CostSummaryReducer,
+    initialCostSummaryState
+  );
 
-    return (
-        <SSRProvider>
-            <UserAuthStateProvider value={{ UserAuthState, dispatchUserAuth }}>
-                <CostSummaryStateProvider
-                    value={{ CostSummaryState, dispatchCostSummary }}
-                >
-                    <BookingContextProvider>
-                        <main className={`${OpenSans.className} container-fluid`}>
-                            <Layout>
-                                <Component {...pageProps} />
-                            </Layout>
-                        </main>
-                    </BookingContextProvider>
-                </CostSummaryStateProvider>
-            </UserAuthStateProvider>
-        </SSRProvider>
-    );
+  return (
+    <SSRProvider>
+      <CostSummaryStateProvider
+        value={{ CostSummaryState, dispatchCostSummary }}
+      >
+        <BookingContextProvider>
+          <main className={`${OpenSans.className} container-fluid`}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </main>
+        </BookingContextProvider>
+      </CostSummaryStateProvider>
+    </SSRProvider>
+  );
 }
